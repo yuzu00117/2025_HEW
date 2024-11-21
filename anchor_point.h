239d8b933689b66ec2b8c8f5ddde53b4f30da17f
Yoshiya1010 <naganoyoshiya721@gmail.com>
2024-11-21 21:19:40 +0900

    furure_anchor_point
    機能追加
    anchor.cpp　ｈの追加
    
    それに伴うfield.cppでの
    各Initなどにanchor_pointなどの関数の呼び出しを設定
    
    player.cpp　ｈの機能変更
    Initの中でnewで読んでいたがそれを変更
    Getインスタンスの段階で初期値を入力しておき　
    コンストラクタの段階でプレイヤーを登録することにした。
    理由としてはインスタンスが重複してできており、GetSizeなどの関数が機能していなかった
    
    Playerのコンストラクタの機能追加
    プレイヤーを中心としたアンカーポイントを検知するセンサーの作成
    
    player.cpp Updateないに、センサーないのアンカーポイントを選択する関数の呼び出しを追加
    
    field.cppのinit内に
    case2にanchor_pointをNewで追加
    
    tool.cpp　及びhに
    値を入力したら絶対値を変換する関数の追加
    
    本日の
    github管理えぐそう
    ゆずっちにぎっとはぶゆずってよかった！

---
+++ anchor_point.h
@@ -0,0 +1,48 @@
+#ifndef ANCHOR_POINT
+#define ANCHOR_POINT
+#include"field.h"
+#include<vector>
+
+
+class AnchorPoint:public Field
+{
+public:
+	AnchorPoint(b2Vec2 position, b2Vec2 body_size, float angle, bool bFixed, bool is_sensor, FieldTexture texture);
+	~AnchorPoint();
+
+
+
+	/**
+	 * @brief  センサーに入ったらVectorにアンカーポイントのBodyを渡してプッシュする
+	 * @param body センサーに入ったアンカーポイントの body
+	 */
+	static void InsideSensor(b2Body* new_anchor_point_body);
+
+
+	/**
+	 * @brief  センサーからでたアンカーポイントのBodyとVectorないのアンカーポイントのボディを比較して
+	 　対応しているものをVectorから消す
+	 * @param body センサーからでたアンカーポイントの body
+	 */
+	static void OutsideSensor(b2Body* delete_anchor_point_body);
+
+	/**
+	 * @brief センサー内のアンカーポイントの中から　
+			  スティックの操作でアンカーポイントを選択する
+	 * @param  
+	 */
+	static void SelectAnchorPoint(float stick_x,float stick_y);
+
+
+	static void Initialize();
+	static void Update();
+	static void Draw();
+
+private:
+	std::vector<b2Body*>InSensorAP;
+};
+
+
+
+#endif // !ANCHOR_POINT
+
---
+++ tool.cpp
@@ -0,0 +1,91 @@
+//-----------------------------------------------------------------------------------------------------
+// #name tool.h
+// #description Floatの計算をかんたんにするため
+// #make 2024/11/02　　永野義也
+// #update 2024/11/02
+// #comment 追加・修正予定
+//          ・floatを簡単に 
+// 　　　　 ・中身は理解してる　でもそんなむずいことしてない　使いやすくしてるだけ
+//          
+//----------------------------------------------------------------------------------------------------
+
+#include"main.h"
+#include"include/box2d/box2d.h"
+#include"tool.h"
+
+
+
+// ==================================================
+// Float2に変換 [◆]
+// ==================================================
+XMFLOAT2 toFloat2(const XMFLOAT3& f) {
+    return XMFLOAT2(f.x, f.y);
+}
+
+XMFLOAT2 toFloat2(const XMFLOAT4& f) {
+    return XMFLOAT2(f.x, f.y);
+}
+
+XMFLOAT2 toFloat2(const XMINT2& f) {
+    return XMFLOAT2(static_cast<float>(f.x), static_cast<float>(f.y));
+}
+
+XMFLOAT2 toFloat2(const b2Vec2& f) {
+    return XMFLOAT2(f.x, f.y);
+}
+
+// ==================================================
+// Float3に変換 [◆]
+// ==================================================
+XMFLOAT3 toFloat3(const XMFLOAT2& f) {
+    return XMFLOAT3(f.x, f.y, 0.0f);
+}
+
+XMFLOAT3 toFloat3(const XMFLOAT4& f) {
+    return XMFLOAT3(f.x, f.y, f.z);
+}
+
+XMFLOAT3 toFloat3(const XMINT2& f) {
+    return XMFLOAT3(static_cast<float>(f.x), static_cast<float>(f.y), 0.0f);
+}
+
+XMFLOAT3 toFloat3(const b2Vec2& f) {
+    return XMFLOAT3(f.x, f.y, 0.0f);
+}
+
+// ==================================================
+// Float4に変換 [◆]
+// ==================================================
+XMFLOAT4 toFloat4(const XMFLOAT2& f) {
+    return XMFLOAT4(f.x, f.y, 0.0f, 1.0f);
+}
+
+XMFLOAT4 toFloat4(const XMFLOAT3& f) {
+    return XMFLOAT4(f.x, f.y, f.z, 1.0f);
+}
+
+XMFLOAT4 toFloat4(const XMINT2& f) {
+    return XMFLOAT4(static_cast<float>(f.x), static_cast<float>(f.y), 0.0f, 1.0f);
+}
+
+XMFLOAT4 toFloat4(const b2Vec2& f) {
+    return XMFLOAT4(f.x, f.y, 0.0f, 1.0f);
+}
+
+
+/**
+ * @brief 絶対値を返す関数
+ * @param value 入れたい値
+ * @return 絶対値
+ */
+float ReturnAbsoluteValue(float value)
+{
+    if (0 < value)//０より大きいため受け取った値をそのまま返す
+    {
+        return value;
+    }
+    else 
+    {
+        return -value;
+    }
+}
