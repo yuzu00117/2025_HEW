1e2811fe76e71ccb13fafeeed8be100d4b1cb4c7
yuzu00117 <yuzu00117.ponzoo@gmail.com>
2024-11-22 20:06:06 +0900

    collider_type.hとcontactlist.hを追加。コライダーのタイプで当たり判定を取れるように

---
+++ contactlist.h
@@ -0,0 +1,140 @@
+#ifndef CONTACTLIST_H
+#define CONTACTLIST_H
+
+#include"include/box2d/box2d.h"
+#include"collider_type.h"
+#include"anchor_point.h"
+
+
+
+
+class MyContactListener : public b2ContactListener {
+public:
+   
+
+
+    // シングルトンのインスタンスを取得する
+    static MyContactListener& GetInstance() {
+        static MyContactListener instance;
+        return instance;
+    }
+
+
+
+
+    // 衝突した瞬間
+    void BeginContact(b2Contact* contact) override {
+
+
+        // 衝突したフィクスチャを取得
+        b2Fixture* fixtureA = contact->GetFixtureA();
+        b2Fixture* fixtureB = contact->GetFixtureB();
+        if (!fixtureA || !fixtureB) return; // NULLチェック
+       
+
+
+       
+        // それぞれのボディからユーザーデータを取得
+          // ボディのユーザーデータを取得
+        auto* objectA = reinterpret_cast<ObjectData*>(fixtureA->GetUserData().pointer);
+        auto* objectB = reinterpret_cast<ObjectData*>(fixtureB->GetUserData().pointer);
+        if (!objectA || !objectB)return;//NULLチェック
+
+    
+
+        // プレーヤーと地面が衝突したかを判定
+        if ((objectA->collider_type == collider_player && objectB->collider_type == collider_ground) ||
+            (objectA->collider_type == collider_ground && objectB->collider_type == collider_player)) {
+            // 衝突処理（プレーヤーと地面が接触した時）
+            
+            int i = 0;
+            // 必要な処理をここに記述
+
+
+        }
+
+
+        //プレイヤーに付属しているセンサーとアンカーポイントが触れた場合
+        if ((objectA->collider_type == collider_player_sensor && objectB->collider_type == collider_anchor_point) ||
+            (objectA->collider_type == collider_anchor_point && objectB->collider_type == collider_player_sensor))
+        {
+            //ぶつかったオブジェクトがどっちがアンカーポイントかわからないので比較する
+
+            b2Body* anchor_point_body;//一時的な入れ物 この中にアンカーポイントのボディをいれる
+
+            if (objectA->collider_type == collider_anchor_point)
+            {
+                //objectAがアンカーポイントだった
+                //のでfixtureAがフィクスチャだよね
+                anchor_point_body=fixtureA->GetBody();
+            }
+            else
+            {
+                //objectBがアンカーポイントだった
+                //のでfixtureBがフィクスチャだよね
+                anchor_point_body = fixtureB->GetBody();
+            }
+            //どちらがアンカーポイントか分かったのでアンカーポイントを保持しておく配列にいれる
+            AnchorPoint::InsideSensor(anchor_point_body);
+           
+        }
+        
+     
+    }
+
+    // 衝突終了時
+    void EndContact(b2Contact* contact) override {
+        // 衝突したフィクスチャを取得
+        // 衝突したフィクスチャを取得
+        b2Fixture* fixtureA = contact->GetFixtureA();
+        b2Fixture* fixtureB = contact->GetFixtureB();
+        if (!fixtureA || !fixtureB) return; // NULLチェック
+
+
+        // それぞれのボディからユーザーデータを取得
+          // ボディのユーザーデータを取得
+        auto* objectA = reinterpret_cast<ObjectData*>(fixtureA->GetUserData().pointer);
+        auto* objectB = reinterpret_cast<ObjectData*>(fixtureB->GetUserData().pointer);
+        if (!objectA || !objectB)return;//NULLチェック
+
+        // プレーヤーと地面が衝突したかを判定
+        if ((objectA->collider_type == collider_player && objectB->collider_type == collider_ground) ||
+            (objectA->collider_type == collider_ground && objectB->collider_type == collider_player)) {
+            // 衝突処理（プレーヤーと地面が接触した時）
+
+
+            // 必要な処理をここに記述
+        }
+
+
+
+        //プレイヤーに付属しているセンサーとアンカーポイントが触れ終わった　（センサーの範囲外にでた）
+        if ((objectA->collider_type == collider_player_sensor && objectB->collider_type == collider_anchor_point) ||
+            (objectA->collider_type == collider_anchor_point && objectB->collider_type == collider_player_sensor))
+        {
+            //ぶつかったオブジェクトがどっちがアンカーポイントかわからないので比較する
+
+            b2Body* anchor_point_body;//一時的な入れ物 この中にアンカーポイントのボディをいれる
+
+            if (objectA->collider_type == collider_anchor_point)
+            {
+                //objectAがアンカーポイントだった
+                //のでfixtureAがフィクスチャだよね
+                anchor_point_body = fixtureA->GetBody();
+            }
+            else
+            {
+                //objectBがアンカーポイントだった
+                //のでfixtureBがフィクスチャだよね
+                anchor_point_body = fixtureB->GetBody();
+            }
+            //どちらがアンカーポイントか分かったのでアンカーポイントを保持しておく配列にいれる
+            AnchorPoint::OutsideSensor(anchor_point_body);
+
+        }
+    }
+
+};
+
+
+#endif // !CONTACTLIST_H
