bea2c3721850223d9b2e872a37ecc0cd88dd5dc6
yuzu00117 <yuzu00117.ponzoo@gmail.com>
2024-12-01 02:54:00 +0900

    fieldのコードをリファクタリング 不必要なコードを削除

--- field.h
+++ field.h
@@ -2,7 +2,7 @@
 // #name field.h
 // #description field.h
 // #make 2024/11/04　永野義也
-// #update 2024/11/18
+// #update 2024/12/01
 // #comment 追加・修正予定
 //          ・Gameないの管理している　基本的にすべての関数がここにたどり着くことに
 //           
@@ -43,30 +43,20 @@ public:
 	static void Draw();
 	static void Finalize();
 
-	// cavファイルを読み込む
+	// csvファイルを読み込み、二次元配列として格納
 	static bool LoadCSV(const std::string& filename);
 
 	// サイズの取得と設定
 	b2Vec2 GetSize() const { return m_size; }
-	void SetSize(const b2Vec2 size) 
-	{
-		m_size = size;
-	}
+	void SetSize(const b2Vec2 size) { m_size = size; }
 
-	//bodyの取得
+	//bodyの取得と設定
 	b2Body* GetFieldBody(void){return m_body;}
-	void SetFieldBody(b2Body* field_body)
-	{
-		m_body = field_body;
-	}
+	void SetFieldBody(b2Body* field_body) { m_body = field_body; }
 
-
-	//フィールドのテクスチャのゲッター　セッター
+	//フィールドのテクスチャのゲッターとセッター
 	FieldTexture  GetFieldTexture(void){return texture;}
-	void SetFieldTexture(const FieldTexture field_texture)
-	{
-		texture = field_texture;
-	}
+	void SetFieldTexture(const FieldTexture field_texture) { texture = field_texture; }
 
 private:
 
@@ -76,21 +66,18 @@ private:
 	//表示に使用するサイズ
 	b2Vec2 m_size;
 
-
 	//フィールドのテクスチャを管理する関数
 	FieldTexture texture;
 
 	// 2次元マップデータを保持する変数
     static std::vector<std::vector<int>> m_field_data;  // CSV読み込み後のマップデータ
 
-
-	static Field*** m_p_field_array; // 2次元配列へのポインタ
-
+	// 2次元配列へのポインタ
+	static Field*** m_p_field_array;
+	
+	//フィールドの幅と高さ
 	static int m_field_width;
 	static int m_field_height;
-
-
-	
 };
 
 
