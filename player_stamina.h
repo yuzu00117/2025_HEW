d68a31169de1bcb7fe3dc20330fd72f8442dae1b
wongwingsum <ths30614@ths.hal.ac.jp>
2024-12-03 20:50:48 +0900

    Playerの体力の管理（ソウル値とはまだ連携していない）

---
+++ player_stamina.h
@@ -0,0 +1,39 @@
+//-----------------------------------------------------------------------------------------------------
+// #name player_stamina.h
+// #description プレイヤーの体力値
+// #make 2024/11/20			王泳心
+// #update 2024/11/20
+// #comment 追加・修正予定
+//
+//           ・残機と画面遷移追加されたら、体力0になった時の処理を追加
+// 
+//----------------------------------------------------------------------------------------------------
+
+
+#pragma once
+
+#ifndef PLAYER_STAMINA_H
+#define	PLAYER_STAMINA_H
+
+#define		MAX_STAMINA		(300.0)
+#define		STAMINA_DAMAGE_BY_OBJECT_LEVEL_1	(-50.0)
+#define		STAMINA_DAMAGE_BY_OBJECT_LEVEL_2	(-100.0)
+
+class PlayerStamina
+{
+public:
+	//今のプレイヤーの体力値をもらう
+	static float	GetPlayerStaminaValue();
+
+	//今のプレイヤーの体力値をマイナス、もしくはプラス（マイナスしたい場合は負数を渡してね！）
+	static void	EditPlayerStaminaValue(float value);
+private:
+	PlayerStamina(){}
+	~PlayerStamina(){}
+
+	//プレイヤーの体力
+	static  float	m_stamina;
+};
+
+#endif // !PLAYER_STAMINA_H
+
