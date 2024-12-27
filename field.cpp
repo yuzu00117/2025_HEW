27497debd4fea2eba3a7d92f609e96a634edbd77
wongwingsum <ths30614@ths.hal.ac.jp>
2024-12-27 16:31:20 +0900

    Update field.cpp
    引っ張れるアンカーレベルを3に

--- field.cpp
+++ field.cpp
@@ -163,7 +163,7 @@ void Field::Initialize()
 			}
 
 			if (field_map[y][x] == 18) {//
-				objectManager.AddMovable_Ground(b2Vec2(x / BOX2D_SCALE_MANAGEMENT, y / BOX2D_SCALE_MANAGEMENT), b2Vec2(4.0f, 5.0f), b2Vec2(1.0f, 1.0f), 1);
+				objectManager.AddMovable_Ground(b2Vec2(x / BOX2D_SCALE_MANAGEMENT, y / BOX2D_SCALE_MANAGEMENT), b2Vec2(5.0f, 5.0f), b2Vec2(1.0f, 1.0f), 3);
 			}
 
 		}
