aaa0d199cfa069fc628a571cf3346216b5a310d7
Yoshiya1010 <naganoyoshiya721@gmail.com>
2025-02-02 17:29:20 +0900

    スタン状態のエフェクトを追加

--- 1-1_boss.cpp
+++ 1-1_boss.cpp
@@ -1072,7 +1072,7 @@ void Boss_1_1::Draw()
 
 			GetDeviceContext()->PSSetShaderResources(0, 1, &g_boss_panic_sheet_Texture);
 
-			DrawDividedSpriteBoss(XMFLOAT2(draw_x, draw_y), 0.0f, XMFLOAT2(GetBossDrawSize().x * scale, GetBossDrawSize().y * scale), 7, 7, sheet_cnt, boss_alpha, left_flag);
+			DrawDividedSpriteBoss(XMFLOAT2(draw_x, draw_y), 0.0f, XMFLOAT2(GetBossDrawSize().x * scale, GetBossDrawSize().y * scale), 16, 17, sheet_cnt, boss_alpha, left_flag);
 
 			break;
 		case jump_state:
@@ -1274,17 +1274,17 @@ void Boss_1_1::UpdateEffectSheetCnt()
 {
 
 	//ピヨピヨ
-	if (panic_effect_sheet_cnt != 0)
-	{
-		panic_effect_sheet_cnt += 0.5;
+	//if (panic_effect_sheet_cnt != 0)
+	//{
+	//	panic_effect_sheet_cnt += 0.5;
 
-		if (Max_panic_effect_sheet_cnt<panic_effect_sheet_cnt)
-		{
-			panic_effect_sheet_cnt = 0;
-		}
+	//	if (Max_panic_effect_sheet_cnt<panic_effect_sheet_cnt)
+	//	{
+	//		panic_effect_sheet_cnt = 0;
+	//	}
 
-		
-	}
+	//	
+	//}
 
 	//チャージ攻撃の攻撃時のエフェクト
 	if (charge_attack_effect_sheet_cnt != 0)
@@ -1425,24 +1425,24 @@ void Boss_1_1::EffectDraw()
 		DrawDividedSpriteBoss(XMFLOAT2(break_draw_x, break_draw_y), 0.0f, XMFLOAT2(GetMiniGolemDrawSize().x * scale * 1.3*1.5, GetMiniGolemDrawSize().y * scale * 1.7*1.5), 4, 2, mini_golem_break_effect_cnt/4, effect_alpha, 1);
 	}
 
-	//ピヨピヨの表示
-	if (panic_effect_sheet_cnt != 0)
-	{
-		//シェーダリソースを設定
-		GetDeviceContext()->PSSetShaderResources(0, 1, &g_boss_panic_effect);
+	////ピヨピヨの表示
+	//if (panic_effect_sheet_cnt != 0)
+	//{
+	//	//シェーダリソースを設定
+	//	GetDeviceContext()->PSSetShaderResources(0, 1, &g_boss_panic_effect);
 
-		b2Vec2 panic_pos = m_body->GetPosition();
+	//	b2Vec2 panic_pos = m_body->GetPosition();
 
-		// プレイヤー位置を考慮してスクロール補正を加える
-		//取得したbodyのポジションに対してBox2dスケールの補正を加える
-		float panic_draw_x = ((panic_pos.x - PlayerPosition::GetPlayerPosition().x) * BOX2D_SCALE_MANAGEMENT) * scale + screen_center.x;
-		float panic_draw_y = ((panic_pos.y - PlayerPosition::GetPlayerPosition().y-((reality_boss_size.y/2)/BOX2D_SCALE_MANAGEMENT)) * BOX2D_SCALE_MANAGEMENT) * scale + screen_center.y;
-
-
+	//	// プレイヤー位置を考慮してスクロール補正を加える
+	//	//取得したbodyのポジションに対してBox2dスケールの補正を加える
+	//	float panic_draw_x = ((panic_pos.x - PlayerPosition::GetPlayerPosition().x) * BOX2D_SCALE_MANAGEMENT) * scale + screen_center.x;
+	//	float panic_draw_y = ((panic_pos.y - PlayerPosition::GetPlayerPosition().y-((reality_boss_size.y/2)/BOX2D_SCALE_MANAGEMENT)) * BOX2D_SCALE_MANAGEMENT) * scale + screen_center.y;
 
 
-		DrawDividedSpriteBoss(XMFLOAT2(panic_draw_x, panic_draw_y), 0.0f, XMFLOAT2(panic_effect_size.x * scale , panic_effect_size.y * scale ), 10, 13, panic_effect_sheet_cnt / 4, effect_alpha, 1);
-	}
+
+
+	//	DrawDividedSpriteBoss(XMFLOAT2(panic_draw_x, panic_draw_y), 0.0f, XMFLOAT2(panic_effect_size.x * scale , panic_effect_size.y * scale ), 10, 13, panic_effect_sheet_cnt / 4, effect_alpha, 1);
+	//}
 }
 
 void Boss_1_1::Finalize()
