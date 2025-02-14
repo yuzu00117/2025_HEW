03ef4b62508144a213bb8e29d8ec8b56262c588c
Yoshiya1010 <naganoyoshiya721@gmail.com>
2025-02-14 18:12:05 +0900

    オブジェクトの衝突時のダメージを消した

--- contactlist.h
+++ contactlist.h
@@ -232,7 +232,7 @@ public:
             }
         }
 
-        // プレーヤーとテレポートブロックが衝突したかを判定
+        // プレーヤーとオブジェクトが衝突したかを判定
         if ((objectA->collider_type == collider_player_leg && objectB->collider_type == collider_object) ||
             (objectA->collider_type == collider_player_body && objectB->collider_type == collider_object) ||
             (objectA->collider_type == collider_object && objectB->collider_type == collider_player_body) ||
@@ -252,7 +252,7 @@ public:
             if (1.0f < object_velocity)//ここに入ったらオブジェクトが移動中であり、被弾判定してよい
             {
                
-                player.Player_Damaged(-50, 120);//被弾処理
+                player.Player_Damaged(0, 120);//被弾処理
 
                 if (objectA->collider_type == collider_object)
                 {
@@ -1171,7 +1171,7 @@ public:
             app_atomex_start(Player_Dead_Sound);
             HitStop::StartHitStop(15);
             CameraShake::StartCameraShake(5, 3, 15);
-            player.Player_Damaged(-50, 120);
+            player.Player_Damaged(0, 120);
             
 
         }
