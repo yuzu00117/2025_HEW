861abeeb5e39993f9f34a73dc8f2b9567471b93c
yuzu00117 <yuzu00117.ponzoo@gmail.com>
2025-03-04 18:19:36 +0900

    ボスムービー機能の追加と関連ファイルの更新
    `DX21_PuzzleGame.vcxproj` に `boss_movie.cpp` と `boss_movie.h` を追加しました。
    `DX21_PuzzleGame.vcxproj.filters` に `GamePause.cpp`、`Item_DamageValue.cpp`、`boss_movie.cpp` を追加し、`GamePause.h`、`Item_DamageValue.h`、`KeyInput_Flag.h`、`boss_movie.h` を追加しました。
    `boss_movie.cpp` ではボスムービーの再生、ポーズ、スキップ機能を実装し、ムービーの初期化、更新、描画、終了処理を含めました。
    `boss_movie.h` では `BossMovie` クラスを宣言し、ムービー管理のメソッドを提供します。

---
+++ boss_movie.cpp
@@ -0,0 +1,169 @@
+//-----------------------------------------------------------------------------------------------------
+// #name boss_movie.cpp
+// #description ボスの登場ムービーを再生するcppファイル
+// #make 2025/03/04　yuzu00117
+// #update 2025/03/04
+// #comment 追加・修正予定
+//          
+//----------------------------------------------------------------------------------------------------
+#include "boss_movie.h"
+#include "keyboard.h"
+#include "scene.h"
+#include "sprite.h"
+#include "texture.h"
+#include"Xinput.h"
+#include"Xinput_controller.h"
+
+static ID3D11ShaderResourceView* g_pause_Texture = NULL;//アンカーのテクスチャ
+static ID3D11ShaderResourceView* g_skip_texture = NULL;//アンカーのテクスチャ
+static ID3D11ShaderResourceView* g_skip_button_texture = NULL;//アンカーのテクスチャ
+
+static bool g_pressed = false;
+
+BossMovie::BossMovie()
+{
+}
+
+BossMovie::~BossMovie()
+{
+}
+
+void BossMovie::Initialize()
+{
+	video.Initialize("asset/movie/op.mp4", false);
+	g_pause_Texture = InitTexture(L"asset\\texture\\sample_texture\\video_pause.png");
+	g_skip_texture = InitTexture(L"asset\\texture\\sample_texture\\skip.png");
+	g_skip_button_texture = InitTexture(L"asset\\texture\\sample_texture\\skip_a.png");
+
+
+	m_pause = false;
+	
+}
+
+void BossMovie::Update()
+{
+	bool finished = video.Update();
+
+	if (finished)
+	{
+		SceneManager& sceneManager = SceneManager::GetInstance();
+
+
+		sceneManager.ChangeScene(SCENE_GAME);
+		return;
+
+	}
+#ifdef _DEBUG
+	if (!g_pressed && Keyboard_IsKeyDownTrigger(KK_X))
+	{
+		Video_State state = video.GetState();
+		if (state == Video_Start)
+		{
+			video.SetState(Video_Pause);
+			m_pause = !m_pause;
+		}
+		else if (state == Video_Pause)
+		{
+			video.SetState(Video_Resume);
+			m_pause = !m_pause;
+		}
+		else if (state == Video_Resume)
+		{
+			video.SetState(Video_Pause);
+			m_pause = !m_pause;
+		}
+	}
+	g_pressed = Keyboard_IsKeyDownTrigger(KK_X);
+
+	if (Keyboard_IsKeyDownTrigger(KK_S))
+	{
+		video.SetState(Video_Skip);
+	}
+#endif // _DEBUG
+
+	//コントローラーの入力の受け取り
+	ControllerState state = GetControllerInput();
+	if (Keyboard_IsKeyDown(KK_SPACE) || (state.buttonA))
+	{
+		skip_cnt += 0.5;
+	}
+	else
+	{
+		if (skip_cnt != 0)
+		{
+			skip_cnt -= 0.5f;
+		}
+
+		if (skip_cnt < 0)
+		{
+			skip_cnt = 0;
+		}
+	}
+
+	if (30 < skip_cnt)
+	{
+  		video.SetState(Video_Skip);
+		skip_cnt = 0;
+	}
+	
+}
+
+void BossMovie::Draw()
+{
+	//バッファクリア
+	Clear();
+
+	if (m_pause)
+	{
+		// シェーダリソースを設定
+		GetDeviceContext()->PSSetShaderResources(0, 1, &g_pause_Texture);
+
+		DrawSpriteOld(
+			{ SCREEN_XCENTER, SCREEN_XCENTER },
+			0.0f,
+			{ 100.0f,100.0f }
+		);
+
+	}
+
+ 	video.Draw(XMFLOAT2(SCREEN_XCENTER, SCREEN_YCENTER),SCREEN_HEIGHT);
+
+	if (g_skip_texture != nullptr)
+	{
+		// シェーダリソースを設定
+		GetDeviceContext()->PSSetShaderResources(0, 1, &g_skip_texture);
+
+		DrawDividedSprite(
+			XMFLOAT2(SCREEN_XCENTER+540, SCREEN_YCENTER + 300),
+			0.0f,
+			XMFLOAT2(200, 70),
+			1, 1, 1
+		);
+	}
+
+	if (g_skip_button_texture != nullptr)
+	{
+		// シェーダリソースを設定
+		GetDeviceContext()->PSSetShaderResources(0, 1, &g_skip_button_texture);
+
+		DrawDividedSprite(
+			XMFLOAT2(SCREEN_XCENTER + 475, SCREEN_YCENTER + 300),
+			0.0f,
+			XMFLOAT2(100, 75),
+			10, 3, skip_cnt
+		);
+	}
+
+	//バックバッファ、フロントバッファ入れ替え
+	Present();
+
+}
+
+void BossMovie::Finalize()
+{
+	video.Finalize();
+	if (g_pause_Texture) { UnInitTexture(g_pause_Texture); }
+	if (g_skip_button_texture) { UnInitTexture(g_skip_button_texture); }
+	if (g_skip_texture) { UnInitTexture(g_skip_texture); }
+	
+}
